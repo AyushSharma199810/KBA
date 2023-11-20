02daebdc0505a48c1954694f082939f89dd5660d
0xanonymous007 <ayush.sharma@eiysys.in>
2023-11-21 00:40:09 +0530

    .

--- Events/profile.go
+++ Events/profile.go
@@ -14,27 +14,27 @@ type Config struct {
 var profile = map[string]Config{
 
 	"manufacturer": {
-		CertPath:     "../Automobile-network/organizations/peerOrganizations/manufacturer.auto.com/users/User1@manufacturer.auto.com/msp/signcerts/cert.pem",
-		KeyDirectory: "../Automobile-network/organizations/peerOrganizations/manufacturer.auto.com/users/User1@manufacturer.auto.com/msp/keystore/",
-		TLSCertPath:  "../Automobile-network/organizations/peerOrganizations/manufacturer.auto.com/peers/peer0.manufacturer.auto.com/tls/ca.crt",
+		CertPath:     "../Land-Reg-network/organizations/peerOrganizations/manufacturer.auto.com/users/User1@manufacturer.auto.com/msp/signcerts/cert.pem",
+		KeyDirectory: "../Land-Reg-network/organizations/peerOrganizations/manufacturer.auto.com/users/User1@manufacturer.auto.com/msp/keystore/",
+		TLSCertPath:  "../Land-Reg-network/organizations/peerOrganizations/manufacturer.auto.com/peers/peer0.manufacturer.auto.com/tls/ca.crt",
 		PeerEndpoint: "localhost:7051",
 		GatewayPeer:  "peer0.manufacturer.auto.com",
 		MSPID:        "ManufacturerMSP",
 	},
 
 	"dealer": {
-		CertPath:     "../Automobile-network/organizations/peerOrganizations/dealer.auto.com/users/User1@dealer.auto.com/msp/signcerts/cert.pem",
-		KeyDirectory: "../Automobile-network/organizations/peerOrganizations/dealer.auto.com/users/User1@dealer.auto.com/msp/keystore/",
-		TLSCertPath:  "../Automobile-network/organizations/peerOrganizations/dealer.auto.com/peers/peer0.dealer.auto.com/tls/ca.crt",
+		CertPath:     "../Land-Reg-network/organizations/peerOrganizations/dealer.auto.com/users/User1@dealer.auto.com/msp/signcerts/cert.pem",
+		KeyDirectory: "../Land-Reg-network/organizations/peerOrganizations/dealer.auto.com/users/User1@dealer.auto.com/msp/keystore/",
+		TLSCertPath:  "../Land-Reg-network/organizations/peerOrganizations/dealer.auto.com/peers/peer0.dealer.auto.com/tls/ca.crt",
 		PeerEndpoint: "localhost:9051",
 		GatewayPeer:  "peer0.dealer.auto.com",
 		MSPID:        "DealerMSP",
 	},
 
 	"mvd": {
-		CertPath:     "../Automobile-network/organizations/peerOrganizations/mvd.auto.com/users/User1@mvd.auto.com/msp/signcerts/cert.pem",
-		KeyDirectory: "../Automobile-network/organizations/peerOrganizations/mvd.auto.com/users/User1@mvd.auto.com/msp/keystore/",
-		TLSCertPath:  "../Automobile-network/organizations/peerOrganizations/mvd.auto.com/peers/peer0.mvd.auto.com/tls/ca.crt",
+		CertPath:     "../Land-Reg-network/organizations/peerOrganizations/mvd.auto.com/users/User1@mvd.auto.com/msp/signcerts/cert.pem",
+		KeyDirectory: "../Land-Reg-network/organizations/peerOrganizations/mvd.auto.com/users/User1@mvd.auto.com/msp/keystore/",
+		TLSCertPath:  "../Land-Reg-network/organizations/peerOrganizations/mvd.auto.com/peers/peer0.mvd.auto.com/tls/ca.crt",
 		PeerEndpoint: "localhost:11051",
 		GatewayPeer:  "peer0.mvd.auto.com",
 		MSPID:        "MvdMSP",
